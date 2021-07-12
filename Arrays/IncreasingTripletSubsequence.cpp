d43e333b682c41d7e46c3d59d08415193e3854a9
Prantik Mukherjee <champmaniac04@gmail.com>
2021-07-12 13:32:20 +0530

    update

--- Arrays/IncreasingTripletSubsequence.cpp
+++ Arrays/IncreasingTripletSubsequence.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int c1=INT_MAX, c2=INT_MAX;
+        int one=INT_MAX, two=INT_MAX;
         for(int x:nums){
-            if(x<=c1)
-                c1=x;
-            else if(x<=c2)
-                c2=x;
+            if(x<=one)
+                one=x;
+            else if(x<=two)
+                two=x;
             else{
                 return true;
             }
