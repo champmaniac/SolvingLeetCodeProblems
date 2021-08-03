7b42937812d0ccc2e5a708799427349a74d7cc8d
Prantik Mukherjee <champmaniac04@gmail.com>
2021-08-03 22:20:26 +0530

    52

---
+++ SubsetII.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res={{}};
+        int startIndex=0;
+        int n=0;
+        for(int i=0;i<nums.size();i++){
+            startIndex= i>=1 && nums[i]==nums[i-1]? n : 0;
+            n=res.size();
+            for(int j=startIndex;j<n;j++){
+                vector<int> temp=res[j];
+                temp.push_back(nums[i]);
+                res.push_back(temp);
+                
+            }
+        }
+        return res;
+    }
+};
