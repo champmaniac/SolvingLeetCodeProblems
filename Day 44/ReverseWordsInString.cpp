9efe606bba16dfed7f2657962b556ee0e5afb645
Prantik Mukherjee <champmaniac04@gmail.com>
2021-08-27 13:41:55 +0530

    76

---
+++ Day 44/ReverseWordsInString.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                int j=i;
+                for(;j<s.size() && s[j]!=' ';j++){}
+                reverse(s.begin()+i,s.begin()+j);
+                i=j-1;
+            }
+            
+        }
+        return s;
+        
+    }
+};
