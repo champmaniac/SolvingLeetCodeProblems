afae34e7d2145cf8768ec17e00e73609c2f6af0c
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-09 11:44:00 +0530

    89

---
+++ Day 57/JumpGame.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n =nums.size();
+        int last=n-1;
+        for(int i=last;i>=0;i--){
+            if(i+nums[i]>=last)
+                last=i;
+        }
+        return last==0;
+    }
+};
