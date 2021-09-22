f79a4160bb1338541a22e932ff2bad0a6d8a1220
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-22 11:34:13 +0530

    102

---
+++ Day 70/ImplementTrie(Prefix Tree).cpp
@@ -0,0 +1,54 @@
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Trie* node =this;
+        for(char ch:word){
+            ch-='a';
+            if(!node->next[ch])
+                node->next[ch] =new Trie();
+            node=node->next[ch];
+        }
+        node->isword =true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie* node =this;
+        for(char ch:word){
+            ch-='a';
+            if(!node->next[ch])
+                return false;
+            node=node->next[ch];
+        }
+        return node->isword;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Trie* node =this;
+        for(char ch:prefix){
+            ch-='a';
+            if(!node->next[ch])
+                return false;
+            node=node->next[ch];
+        }
+        return true;
+    }
+private:
+    Trie* next[26] = {};
+    bool isword = false;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
