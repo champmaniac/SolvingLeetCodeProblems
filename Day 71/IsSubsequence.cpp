f9365c138d05b8ee3a7fb0c8847c7fb64bebad52
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-23 11:48:26 +0530

    103

--- Day 71/IsSubsequence.cpp
+++ Day 71/IsSubsequence.cpp
@@ -1,16 +1,48 @@
 class Solution { // Recursive Approach
 public:
-    bool isSubsequence(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        return isSub(s,t,m,n);
-    }
-    bool isSub(string str1,string str2, int m,int n){
-        if(m==0) return true;
-        if(n==0) return false;
-        if(str1[m-1]==str2[n-1]){
-            return isSub(str1,str2,m-1,n-1);
+    // bool isSubsequence(string s, string t) {
+    //     int m = s.size();
+    //     int n = t.size();
+    //     return isSub(s,t,m,n);
+    // }
+    // bool isSub(string str1,string str2, int m,int n){
+    //     if(m==0) return true;
+    //     if(n==0) return false;
+    //     if(str1[m-1]==str2[n-1]){
+    //         return isSub(str1,str2,m-1,n-1);
+    //     }
+    //     return isSub(str1,str2,m,n-1);
+    // }
+    
+    // DP Approach
+    int helper(string x,string y){
+        int m = x.size();
+        int n = y.size();
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
         }
-        return isSub(str1,str2,m,n-1);
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(x[i-1]==y[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    
+    bool isSubsequence(string s, string t){
+        int x = helper(s,t);
+        if(x==s.size())
+            return true;
+        return false;
     }
 };
