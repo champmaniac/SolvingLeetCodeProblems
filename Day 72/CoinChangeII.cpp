5b8224ce1e64b820ab49622ba539d714df0271b3
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-24 11:19:29 +0530

    104

---
+++ Day 72/CoinChangeII.cpp
@@ -0,0 +1,14 @@
+class Solution { //TC: O(length of coins * amount)
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1);
+        dp[0]=1;
+        for(int i =0;i<coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                if(j-coins[i]>=0)
+                    dp[j]+=dp[j-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+};
