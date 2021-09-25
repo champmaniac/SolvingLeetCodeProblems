6f67023cec05c2e15cee73a0640d591c3e369f28
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-25 09:53:17 +0530

    105

---
+++ Day 73/CombinationSumIV.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(auto num:nums){
+                if(num<=i)
+                    dp[i]+=dp[i-num];
+            }
+        }
+        return dp[target];
+    }
+};
