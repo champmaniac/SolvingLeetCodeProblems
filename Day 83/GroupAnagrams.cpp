7b10b4c171427679409629d8bff90ea71b7877ea
Prantik Mukherjee <champmaniac04@gmail.com>
2021-10-05 10:00:08 +0530

    115

---
+++ Day 83/GroupAnagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<int>> m;
+        for(int i=0;i<strs.size();++i){
+            auto s= strs[i];
+            sort(s.begin(),s.end());
+            m[s].push_back(i);
+        }
+        for(auto a:m){
+            vector<string> v;
+            for(auto b:a.second)
+                v.push_back(strs[b]);
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
