e6d98b522c3b16985144887fcc0a343cbcaa2bcf
Prantik Mukherjee <champmaniac04@gmail.com>
2021-10-22 17:32:45 +0530

    132

---
+++ Day 100/ReverseLinkedlist.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL) return NULL;
+        ListNode *p=NULL,*c=head,*n=head->next;
+        while(c!=NULL){
+            c->next=p;
+            p=c;
+            c=n;
+            if(n!=NULL)
+                n=n->next;
+        }
+        return p;
+    }
+};
