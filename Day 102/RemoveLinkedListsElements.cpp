9658e6d7983b4bcfe5f2994b4a92da6a43f4985b
Prantik Mukherjee <champmaniac04@gmail.com>
2021-10-24 18:54:19 +0530

    134

--- Day 102/RemoveLinkedListsElements.cpp
+++ Day 102/RemoveLinkedListsElements.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    ListNode* removeElements(ListNode* head, int val) {
+    
+    ListNode* removeElements(ListNode* head, int val){ // Recursive
         if(head==NULL) return head;
-        ListNode * dummy = new ListNode(-1);
-        dummy->next =head;
-        ListNode * tail = dummy;
-        while(tail!=NULL && tail->next!=NULL)
-        {
-            if(tail->next->val==val)
-            {
-                ListNode * temp = tail->next;
-                tail->next = temp->next;
-                delete temp;
-            }
-            else
-            {
-                tail=tail->next;
-            }
-        }
-        return dummy->next;
+        head->next = removeElements(head->next,val);
+        return head->val==val?head->next:head;
     }
+    // ListNode* removeElements(ListNode* head, int val) { // Iterative TC O(N) SC O(N
+    //     if(head==NULL) return head;
+    //     ListNode * dummy = new ListNode(-1);
+    //     dummy->next =head;
+    //     ListNode * tail = dummy;
+    //     while(tail!=NULL && tail->next!=NULL)
+    //     {
+    //         if(tail->next->val==val)
+    //         {
+    //             ListNode * temp = tail->next;
+    //             tail->next = temp->next;
+    //             delete temp;
+    //         }
+    //         else
+    //         {
+    //             tail=tail->next;
+    //         }
+    //     }
+    //     return dummy->next;
+    // }
 };
