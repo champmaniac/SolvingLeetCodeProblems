40c604a72abe1d80c6ca7ca4c109a09853f9e197
Prantik Mukherjee <champmaniac04@gmail.com>
2021-11-14 11:40:39 +0530

    153

---
+++ Day 123/WaveArray.cpp
@@ -0,0 +1,26 @@
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(vector<int>& arr, int n){
+        
+        // Your code here
+        if(n%2==0){
+            for(int i=0;i<n-1;i+=2){
+                int temp = arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        else
+        {
+            for(int i=0;i<n-2;i+=2){
+                int temp =arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        
+    }
+};
